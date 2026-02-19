2b5827a08d0f37a938a46c45cbfd3cb46c4d557d
ap963527-cpu <ap963527@gmail.com>
2026-02-19 10:42:18 +0530

    Update Solution.py

--- ALA1/Solution.py
+++ ALA1/Solution.py
@@ -1,1 +1,22 @@
+print("Countdown Program")
+num = int(input("Enter starting number: "))
+sum_total = 0
+
+while num >= 0:
+    print("Number:", num)
+    sum_total = sum_total + num
+
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+    num = num - 1
+
+print("Total sum:", sum_total)
+
+if sum_total > 100:
+    print("Big total")
+else:
+    print("Small total")
 
