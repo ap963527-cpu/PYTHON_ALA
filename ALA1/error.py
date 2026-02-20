42be70830db14b52780861e514ac9078af774dcd
ap963527-cpu <ap963527@gmail.com>
2026-02-20 10:51:46 +0530

    Update error.py

--- ALA1/error.py
+++ ALA1/error.py
@@ -1,17 +1,20 @@
 print("Countdown Program")
 num = int(input("Enter starting number: "))
 sum_total = 0
+
 while num >= 0:
-print("Number:", num)
-sum_total = sum_total + num
-if num % 2 == 0:
-print("Even")
+print("Number:", num)   # Error: No indentation inside while
+sum_total = sum_total + num   # Error: No indentation
+
+if num % 2 == 0:   # Error: This if should be inside while
+print("Even")   # Error: No indentation
 else:
-print("Odd")
-num = num - 1
+print("Odd")   # Error: No indentation
+
+num = num - 1   # Error: Should be inside while loop
 print("Total sum:", sum_total)
 
-if sum_total > 100
-print("Big total")
+if sum_total > 100   # Error: Missing colon :
+print("Big total")   # Error: No indentation
 else:
-print("Small total")
+print("Small total")   # Error: No indentation
